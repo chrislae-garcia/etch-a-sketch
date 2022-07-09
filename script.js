06cf8bfb9eb6368ffc2f28bc2a24273b13aa74c5
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-09 17:35:50 +0800

    Refactor event listener

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const divContainer = document.querySelector('.container');
 console.log(divContainer);
 
-for(let row = 0; row < 16; row++) {
+for (let row = 0; row < 16; row++) {
   const divSquareRow = document.createElement('div');
   divSquareRow.classList.add('row');
   divContainer.appendChild(divSquareRow);
-  for(let col = 0; col < 16; col++) {
+  for (let col = 0; col < 16; col++) {
     const divSquareCol = document.createElement('div');
     divSquareCol.classList.add('col');
     divSquareRow.appendChild(divSquareCol);
   }
 }
 
-window.addEventListener('mouseover', function(hoveredBox) {
-  if(hoveredBox.target.className!='col') return;
-  hoveredBox.target.style.backgroundColor = 'blue';  
-});
+function fillBox(e) {
+  console.log(e);
+  e.target.style.backgroundColor = 'blue';
+
+}
+
+const boxes = document.querySelectorAll('.col');
+boxes.forEach(box => box.addEventListener('mousedown', fillBox));
