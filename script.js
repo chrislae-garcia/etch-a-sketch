8218a9af7688d282d87fc36e164587e267ca762d
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-10 00:06:53 +0800

    Add clearGrid() functionality and fix spacings

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const divContainer = document.querySelector('.container');
+const body = document.querySelector('body');
+
+let divContainer = document.querySelector('.container');
 console.log(divContainer);
 
 const btnEditGrid = document.getElementById('btn-edit-grid');
@@ -6,28 +8,30 @@ const btnEditGrid = document.getElementById('btn-edit-grid');
 function promptGrid() {
   let gridSize = prompt('Set new grid size:');
 
-  if (gridSize === null) {
-    return;
-  }else if (gridSize > 0 && gridSize <= 100) {
-    createGrid(gridSize);  
-  } else {
-    alert('Please input a number between 1 to 100');
-  }
-  
+  if (gridSize === null) return;
+  else if (gridSize > 0 && gridSize <= 100) createGrid(gridSize);
+  else alert('Please input a number between 1 to 100');
 }
 
 btnEditGrid.addEventListener('click', promptGrid);
 
 function clearGrid() {
-  
+  const pixelGrid = document.querySelector('.container');
+  body.removeChild(pixelGrid);
+  console.log('test');
+  divContainer = document.createElement('div');
+  divContainer.classList.add('container');
+  body.appendChild(divContainer);
 }
 
-function createGrid(grid=16) {
-    for (let row = 0; row < grid; row++) {
+function createGrid(gridSize = 16) {
+
+  if (gridSize) clearGrid();
+  for (let row = 0; row < gridSize; row++) {
     const divSquareRow = document.createElement('div');
     divSquareRow.classList.add('row');
     divContainer.appendChild(divSquareRow);
-    for (let col = 0; col < grid; col++) {
+    for (let col = 0; col < gridSize; col++) {
       const divSquareCol = document.createElement('div');
       divSquareCol.classList.add('col');
       divSquareRow.appendChild(divSquareCol);
@@ -37,14 +41,14 @@ function createGrid(grid=16) {
 
 let isDrawing = false;
 // color the pixel box on mouse click
-window.addEventListener('mousedown', function(fillBox) {
-  if (fillBox.target.className != 'col') return; 
+window.addEventListener('mousedown', function (fillBox) {
+  if (fillBox.target.className != 'col') return;
   fillBox.target.style.backgroundColor = 'blue';
   isDrawing = true;
 });
 
 // color the pixel boxes on mouse hold
-window.addEventListener('mouseover', function(fillBox) {
+window.addEventListener('mouseover', function (fillBox) {
   if (fillBox.target.className != 'col') return;
   if (isDrawing) fillBox.target.style.backgroundColor = 'blue';
 });
